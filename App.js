ff18e917b57d43b14c7838020e50beee4532ff87
joep B <joepbarmentlo@gmail.com>
2019-11-06 14:17:54 +0100

    new questions

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Test from './Models/test'
 
+// 0) npm start puis resolve le bug inclus en cadeau
 
-// Pourquoi npm start lance-t-il expo ?
-
-// 1) modifier Test afin d'afficher du texte a l'ecran
+// 1) modifier Test afin que le texte ne soit pas cache par la barre du haut
 
 // 2) passer le texte a afficher dans les props de Test
 
@@ -22,7 +20,10 @@ import Test from './Models/test'
 // 7) enlever le bouton, centrer le texte a l'ecran. 
 // tout l'ecran doit repondre au toucher comme le bouton le faisait avant
 
+// 8) cree un nouveau component NoteList qui affiche une liste de notes a l'ecran
+// et permet d'ajouter des "note" a cette liste (une note est tout simplement une string)
 
+// bonus Pourquoi npm start lance-t-il expo ?
 
 export default class App extends React.Component {
 
