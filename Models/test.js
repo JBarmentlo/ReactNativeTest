a0dd199650bd8403ecfb59b4fd38e8828e7e0e53
joep B <joepbarmentlo@gmail.com>
2019-11-07 14:02:11 +0100

    Correction

--- Models/test.js
+++ Models/test.js
@@ -1,18 +1,104 @@
 
 import React from 'react'
-import { View,  Button, Text} from 'react-native'
+import { View,  StyleSheet, Text, Button, TouchableOpacity} from 'react-native'
 
-
-// 1.1) Le texte est cache par la barre du haut, resolvez cela.
-// 1.2) Comment peut on forcer l'affichage de NEW TEXT ?
-
+/*
 export default class Test1 extends React.Component {
+
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            texto: props.txt
+        }
+    }
 
     render() {
         return(
-
-                <Text>gl hf
-                </Text>
+            <TouchableOpacity style= {styles.center} onPress = {() => {this.setState({texto: "newText"})}}>
+                <Text>{this.state.texto}</Text>
+            </TouchableOpacity>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    center: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }})
+
+*/
+
+/*
+export default class Test1 extends React.Component {
+
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            texto: props.txt
+        }
+    }
+
+    render() {
+        return(
+            <View style= {styles.center}>
+                <Text>{this.state.texto}</Text>
+                <Button title= "press to change" onPress = {() => 
+                {
+                    this.setState({texto: "newText"}) 
+                
+                }}/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    center: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }})
+*/
+
+export default class Test1 extends React.Component {
+
+    constructor(props)
+    {
+        super(props)
+        this.txt = this.props.txt
+    }
+
+    appupdate = () => {
+        this.forceUpdate()
+      }
+    
+    render() {
+        console.log(this.props.txt)
+        return(
+            <View style= {styles.center}>
+                <Text>{this.txt}</Text>
+                <Button title= "press to change" onPress = {() => 
+                {
+                    this.txt = "HJHAD"
+                    this.appupdate()
+                }}>
+                </Button>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    center: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }})
+    
